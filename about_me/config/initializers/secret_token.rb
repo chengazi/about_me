f3a188ac665408d44e523f5a17bb50ff8b928bf5
zorro_chan@VM_Kubuntu <zorro_chan@126.com>
2014-05-06 15:42:09 +0800

    add rails project

---
+++ about_me/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AboutMe::Application.config.secret_token = '9422a3208538a8be90cfdbc102c9e0cf99b34d6df65e5f9b018c21f565a2676c26ea38fbdae048538a24f3bf528ac06f2f373932e5db21fa7f6f90b89fe14a47'
